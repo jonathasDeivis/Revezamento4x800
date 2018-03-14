7556fa5879633873f640186c6eccf78a45e1fc9d
jonathasDeivis <jonathas.araujo@faciplac.edu.br>
2018-03-14 17:38:30 -0300

    Configurando travis Brasil3

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,27 +12,27 @@ RSpec.describe UsersController, type: :controller do
     it "returns a success response" do
       user = User.create(name: "Orcestra", email: "projetos@orcestra.com.br")
       get :index, params: {}, session: valid_session
-      # expect(response).to be_success
-      expect(nil).to be_success
+       expect(response).to be_success
+      #expect(nil).to be_success
     end
   end
 
   describe "POST #create" do
     context "with valid params" do
       it "creates a new User" do
-        # expect {
-        #   post :create, params: {user: valid_attributes}
-        # }.to change(User, :count).by(2)
-        expect(nil).to be_success
+         expect {
+           post :create, params: {user: valid_attributes}
+         }.to change(User, :count).by(2)
+      #  expect(nil).to be_success
       end
 
       it "renders a JSON response with the new user" do
 
         post :create, params: {user: valid_attributes}, session: valid_session
-        # expect(response).to have_http_status(:created)
-        # expect(response.content_type).to eq('application/json')
-        # expect(response.location).to eq(user_url(User.last))
-        expect(nil).to be_success
+         expect(response).to have_http_status(:created)
+         expect(response.content_type).to eq('application/json')
+         expect(response.location).to eq(user_url(User.last))
+       # expect(nil).to be_success
       end
     end
   end
